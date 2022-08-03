542576a819afebff3a3d01e3bdc19bf49e1f62f6
romulo <romulo.henrique@atlastechnol.com>
2022-08-02 22:19:33 -0300

    feat:new function

--- src/app/Repositories/Product/Contract/ProductRepositoryContract.php
+++ src/app/Repositories/Product/Contract/ProductRepositoryContract.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories\Product\Contract;
 
+use App\Models\Product;
+
 
 interface ProductRepositoryContract {
 
@@ -20,4 +22,57 @@ interface ProductRepositoryContract {
      * @return bool
      */
     public function isbnUnique(string $isbn): bool;
+
+    /**
+     * Get all products
+     * @return array
+     */
+    public function getAllProducts(): array;
+
+    /**
+     * Get product by id
+     * @param int $id
+     * @return Product
+     */
+    public function getProductById(int $id): Product;
+
+    /**
+     * Get product by isbn
+     * @param string $isbn
+     * @return Product
+     */
+    public function getProductByIsbn(string $isbn): Product;
+
+    /**
+     * Update product by id
+     * @param int $id
+     * @param string $name
+     * @param string $isbn
+     * @param float $value
+     * @return bool
+     */
+    public function updateProduct(int $id, string $name, string $isbn, float $value): bool;
+
+    /**
+     * Update product by isbn
+     * @param string $isbn
+     * @param string $name
+     * @param float $value
+     * @return bool
+     */
+    public function updateProductByIsbn(string $isbn, string $name, float $value): bool;
+
+    /**
+     * Delete product by id
+     * @param int $id
+     * @return bool
+     */
+    public function deleteProduct(int $id): bool;
+
+    /**
+     * Delete product by isbn
+     * @param string $isbn
+     * @return bool
+     */
+    public function deleteProductByIsbn(string $isbn): bool;
 }

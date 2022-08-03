c58df54c210c9a66b545f2cd857ea6da21762650
romulo <romulo.henrique@atlastechnol.com>
2022-08-03 08:01:36 -0300

    feat: New controlles select

---
+++ src/app/Http/Controllers/SelectProtuctByIdController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Services\Product\SelectProtuctService;
+use Illuminate\Http\Response;
+use Exception;
+
+class SelectProtuctByIdController extends Controller
+{
+
+    /**
+     * @var SelectProtuctService
+     */
+    private $selectProtuctService;
+
+    public function __construct(SelectProtuctService $selectProtuctService)
+    {
+        $this->selectProtuctService = $selectProtuctService;
+    }
+
+    public function __invoke($id)
+    {
+        try{
+            $data = $this->selectProtuctService->getProductById($id);
+            return response()->json(['data'=>$data,
+                                     'status'=>Response::HTTP_OK], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json(['data'=>'Error',
+                                     'status'=>Response::HTTP_BAD_REQUEST], Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
